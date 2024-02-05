45aaff0918edf3b6f303272c92dbf8f46535aba4
lucasmoraist <luksmn1101@gmail.com>
2024-02-05 08:36:15 -0300

    feat: esqueleto do código

---
+++ src/main/java/com/lucas/cafeliterario/CafeLiterarioApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.cafeliterario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeLiterarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeLiterarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/cafeliterario/CafeLiterarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.cafeliterario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeLiterarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
